0b3e5b29532547e7b1cffcf3ac9e63caed22f939
gconeice <yyang811@gatech.edu>
2023-10-11 15:26:32 -0400

    update setupsh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+mkdir setup &&
+cd setup &&
 wget https://raw.githubusercontent.com/emp-toolkit/emp-readme/master/scripts/install.py &&
-python3 install.py --deps --tool --ot --zk
+python3 install.py --deps --tool --ot --zk &&
+cd ..
     
